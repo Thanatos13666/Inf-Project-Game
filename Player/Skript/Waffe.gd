dda6f94836456d39b492af0ff88803749ddd0907
Janos23 <61746912+Janos23@users.noreply.github.com>
2020-06-02 12:34:16 +0200

    Add files via upload

--- Player/Skript/Waffe.gd
+++ Player/Skript/Waffe.gd
@@ -3,23 +3,21 @@ extends Node2D
 
 const rotation_speed = 2
 var r = 90
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-# Called when the node enters the scene tree for the first time.
+
+
+
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
 
 func _process(delta):
-	input_key()
+	Input_key()
+	
 
-
-func input_key():
+func Input_key():
 
 
 	if Input.is_action_pressed("ui_a"):
@@ -53,3 +51,8 @@ func input_key():
 			while r > 360:
 				r = r - 360;
 			set_rotation_degrees(r);
+
+
+
+
+
