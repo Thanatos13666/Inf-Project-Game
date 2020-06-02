c51b5efca740a5a83044030f34b337e8c359ff72
SanderForGodot <61471064+SanderForGodot@users.noreply.github.com>
2020-06-02 12:47:16 +0200

    all in master (#3)
    * Add files via upload
    
    * Delete Inf-Project-Game-Player.rar
    
    * Add files via upload
    
    * Delete Godot_VS.dll
    
    * Delete Blau_1.gdns
    
    * Delete Blau_1.gd
    
    * Delete Blau_1.gdns
    
    * Delete gdnativelibrary.gdnlib
    
    * create new unit tree
    
    * neuses spiel sene aktualiesiert
    
    * Ich hör für heute auf
    
    * Add files via upload
    
    * deleted:    Game.tscn
    
    * Player Ordner fixen
    xxxxx
    
    * Main menu update/ added options scene/ spiel starten möglich
    
    * Add files via upload
    
    * ESC menü hinzugefügt
    
    * mergen
    
    * Add files via upload
    
    * Delete Schuss.tscn
    
    * Delete Explosion.png
    
    * Delete Explosion.png.import
    
    * Delete Flash.png
    
    * Delete Flash.png.import
    
    * Delete Schuss.png.import
    
    * Delete Gun_01.png
    
    * Delete Gun_01.png.import
    
    * Delete Hull_01.png
    
    * Delete Hull_01.png.import
    
    * Delete Blau_1.tscn
    
    * Delete Abschuss.gd
    
    * Delete Blau_1.gd
    
    * Delete Schuss.gd
    
    * Delete Waffe.gd
    
    * Add files via upload
    
    Co-authored-by: Janos23 <61746912+Janos23@users.noreply.github.com>
    Co-authored-by: Josh <energiedragon@gmail.com>

--- units/KinematicBody_Waffe.gd
+++ units/KinematicBody_Waffe.gd
@@ -3,21 +3,23 @@ extends Node2D
 
 const rotation_speed = 2
 var r = 90
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
-
-
-
-
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
-
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
 
 func _process(delta):
-	Input_key()
-	
+	input_key()
 
-func Input_key():
+
+func input_key():
 
 
 	if Input.is_action_pressed("ui_a"):
@@ -51,8 +53,3 @@ func Input_key():
 			while r > 360:
 				r = r - 360;
 			set_rotation_degrees(r);
-
-
-
-
-
