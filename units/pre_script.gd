bfb699af01604b8383fb717c97fec16fb487e069
Josh <energiedragon@gmail.com>
2020-06-11 20:01:53 +0200

    Aaaaaaaaaaaaaaaaaahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- units/pre_script.gd
+++ units/pre_script.gd
@@ -1,6 +1,17 @@
 extends Node2D
 
+signal is_selected(selber)
+
 #base functions-----------------------------------------------------------------
 func die():
 	self.queue_free();
+	pass
+
+func move():
+	
+	pass
+
+
+func _on_Button_pressed():
+	emit_signal("is_selected",self)
 	pass
