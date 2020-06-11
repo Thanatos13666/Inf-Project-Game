8c9e646c5fde8e7ee9705419a411fe6bf0234ef8
SanderForGodot <61471064+SanderForGodot@users.noreply.github.com>
2020-06-11 22:10:02 +0200

    ka

--- game/gui/Shop_Button.gd
+++ game/gui/Shop_Button.gd
@@ -1,10 +1,9 @@
 extends Button
-#
-#
-#
-#func _ready():
-#	_on_Button_toggled(true)
-#func _on_Button_toggled(button_pressed):
-#	#es gibt garantiert ne beserelösung als dieses hier
-#	get_parent().get_parent().get_parent().get_node("shop grop/HBoxContainer/VBoxContainer/u_r/Shop_scroll_con").visible = pressed #sichtbarkeit = bool buton gedrückt
-#
+
+
+
+func _ready():
+	_on_Button_toggled(true)
+func _on_Button_toggled(button_pressed):
+	#es gibt garantiert ne beserelösung als dieses hier
+	get_parent().get_parent().get_parent().get_node("shop grop/HBoxContainer/VBoxContainer/u_r/Shop_scroll_con").visible = pressed #sichtbarkeit = bool buton gedrückt
