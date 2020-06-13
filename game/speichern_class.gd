188cc4923ec835d0b85eab8050728d8d57cb3910
SanderForGodot <61471064+SanderForGodot@users.noreply.github.com>
2020-06-13 21:19:22 +0200

    Speichern Teil1 (noch sieht man nix ist das rückrad)

---
+++ game/speichern_class.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+
+class_name Savegame
+
+export var version : String =ProjectSettings.get_setting('application/config/version')
+export var data :Dictionary ={}
+
--- units/pre_unit.gd
+++ units/pre_unit.gd
@@ -32,7 +32,7 @@ var base_values = {
 #######################################################################################
 var curr_values = {
 	"Leben":base_values.Leben,
-	"Angriff":base_values.Angriff,
+	"Angriff":0,
 	"Ausweichen":base_values.Ausweichen,
 	"Reichweite":base_values.Reichweite,
 	"Bewegungsrate":base_values.Bewegungsrate,
@@ -41,7 +41,8 @@ var curr_values = {
 var func_list = {
 	"reset_values":funcref(self,"reset_values"),
 	"test_A":funcref(self,"test_A"),
-	"test_B":funcref(self,"test_B")
+	"test_B":funcref(self,"test_B"),
+	"getDmg":funcref(self,"getDmg")
 }
 
 #test funktionen
@@ -50,6 +51,43 @@ func test_A():
 func test_B():
 	print("test_B")
 
+#Speichern und Laden------------------------------------------------------------
+
+#varieabeln die abgespeichert werden sollen
+var save_me={#hier kommt rein was wirklich JEDE unterklsse hat
+	"Typ":"pre_unit",		#muss in unterklassen überschrieben werden
+	"pos":Vector2(0,0),
+	"Leben":0,
+	"Angriff":0,
+	"Ausweichen":0,
+	"Reichweite":0,
+	"Bewegungsrate":0,
+}
+
+func save(save_res):
+	save_me.pos = position
+	save_me.Leben = curr_values.Leben
+	save_me.Angriff = curr_values.Angriff
+	save_me.Ausweichen = curr_values.Ausweichen
+	save_me.Reichweite = curr_values.Reichweite
+	save_me.Bewegungsrate = curr_values.Bewegungsrate
+	
+	speacial_save()
+	
+	save_res.subdata[get_index()]= save_me
+	
+func speacial_save(): #wird in den deinzelnen klassen ausdiferenziert
+	pass
+	
+func laden(values):
+	position = values.pos 
+	curr_values.Leben = values.Leben
+	curr_values.Angriff = values.Angriff
+	curr_values.Ausweichen = values.Ausweichen
+	curr_values.Reichweite = values.Reichweite
+	curr_values.Bewegungsrate = values.Bewegungsrate
+	
+	
 #base functions-----------------------------------------------------------------
 func reset_values():
 	curr_values=base_values #es muss überprüft werdenob das geht
@@ -76,7 +114,9 @@ func _on_Button_pressed():#Button ist hier die figur je selbst
 		print("select")
 	pass
 
-func getDmg(dmg):
+func getDmg():
+	var dmg=0
+	print("std get dmg")
 	curr_values.Leben -= dmg;
 	if curr_values.Leben <= 0:
 		die();
